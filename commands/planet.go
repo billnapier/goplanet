124a61934c2a88d7f3c617c889dc30d91a426fc8
Bill Napier <napier@pobox.com>
2014-07-21 10:38:00 -0700

    Now we can read in a config file.

---
+++ commands/planet.go
@@ -0,0 +1,47 @@
+package commands
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var RootCmd = &cobra.Command{
+	Use:   "goplanet",
+	Short: `planet style RSS aggregator`,
+	Long:  `Provides a planet style RSS aggregator`,
+	Run:   rootRun,
+}
+
+func rootRun(cmd *cobra.Command, args []string) {
+	fmt.Println(viper.GetString("appname"))
+	fmt.Println(viper.Get("feeds"))
+}
+
+func Execute() {
+	err := RootCmd.Execute()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+}
+
+var CfgFile string
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	RootCmd.PersistentFlags().StringVar(&CfgFile, "config", "",
+		"config file (default is $HOME/.goplanet/config.yaml)")
+}
+
+func initConfig() {
+	if CfgFile != "" {
+		viper.SetConfigFile(CfgFile)
+	}
+	viper.SetConfigName("config")
+	viper.AddConfigPath("/etc/goplanet")
+	viper.AddConfigPath("$HOME/.goplanet")
+	viper.ReadInConfig()
+}
